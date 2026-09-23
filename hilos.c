6988bc84dd385f4aa044b69483b8bdd6dca42350
acatarip <acatarip@unjbg.edu.pe>
2026-09-23 10:06:32 -0500

    Implementacion de sincronizacion

--- hilos.c
+++ hilos.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+pthread_mutex_t mutex;
+
+//hilo funcion con mutex
 void* hilo_funcion(void* arg){
+  pthread_mutex_lock(&mutex);
   printf("Hola desde el hilo %ld\n", (long)arg);
+  pthread_mutex_unlock(&mutex);
   pthread_exit(NULL);
 }
+
 int main(){
   pthread_t hilo1, hilo2;
+  pthread_mutex_init(&mutex, NULL);
+  
   pthread_create(&hilo1, NULL, hilo_funcion, (void*)1);
   pthread_create(&hilo2, NULL, hilo_funcion, (void*)2);
   
   pthread_join(hilo1, NULL);
   pthread_join(hilo2, NULL);
   
+  pthread_mutex_destroy(&mutex);
   printf("Finalizo la ejecucion del programa principal\n");
   return 0;
 }
